8aa4a684ce4b182f2dad6e6b442cc97a8fdbb1ba
helboi4 <h.hopecarter@gmail.com>
2021-10-22 14:31:43 +0200

    Added FlightService class with empty methods

---
+++ src/main/java/com/airhoot/Flights/FlightService.java
@@ -0,0 +1,24 @@
+package com.airhoot.Flights;
+
+public class FlightService {
+    public static boolean addPassenger(){
+
+    };
+
+    public static boolean removePassenger(){
+
+    };
+
+    public static boolean changeStatus{
+
+    };
+
+    public static boolean addFlightToList(){
+
+    };
+
+    public static boolean cancelFlight(){
+
+    };
+
+}
