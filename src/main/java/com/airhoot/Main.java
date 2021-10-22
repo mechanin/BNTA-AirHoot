8a4cb894fb89d65a4c0219605b864066b1e8cf65
mechanin <86329519+mechanin@users.noreply.github.com>
2021-10-22 17:11:05 +0100

    Added menu GUI framework

--- src/main/java/com/airhoot/Main.java
+++ src/main/java/com/airhoot/Main.java
@@ -1,5 +1,7 @@
 package com.airhoot;
 
+import com.airhoot.Flights.FlightService;
+
 import java.util.Scanner;
 
 public class Main {
@@ -15,8 +17,13 @@ public static void main(String[] args) {
         System.out.println("Select function to perform: ");
         System.out.println("(1)CREATE NEW FLIGHT");
         System.out.println("(2)EDIT EXISTING FLIGHT");
-        System.out.println("(3)CANCEL EXISTING");
+        System.out.println("(3)CANCEL EXISTING FLIGHT");
         int adminSelection = sc.nextInt();
+        switch(adminSelection) {
+            case 1:
+                FlightService.addFlight();
+                break;
+        }
 
 //      if mainSelection is 2 display booking options
     } else if (selection == 2) {
@@ -25,8 +32,12 @@ public static void main(String[] args) {
         System.out.println("(2)EDIT/CANCEL A BOOKING");
         int bookingSelection = sc.nextInt();
     }
-
 //     if mainSelection is not 1 or 2 prompt a proper input
+    else {
+        System.out.println("Please enter a number between 1 and 2");
+    }
+
+
 
 //      if selection is 2 display booking options
 }
