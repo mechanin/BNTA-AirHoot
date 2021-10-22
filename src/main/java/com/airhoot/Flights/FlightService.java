5133fc479c238fe2fde2888435d1fb8fe9808901
helboi4 <h.hopecarter@gmail.com>
2021-10-22 15:59:55 +0200

    add passenger etc

--- src/main/java/com/airhoot/Flights/FlightService.java
+++ src/main/java/com/airhoot/Flights/FlightService.java
@@ -1,16 +1,23 @@
-package com.airhoot.Flights;
 
+
+import com.airhoot.Flights.Flight;
+import com.airhoot.Flights.Status;
 import com.airhoot.person.Person;
 
 public class FlightService {
+    //Flight flight  = new Flight("Paris", "London", 2021-11-06, 180, 200, 150, 0, Status.ON_TIME);
     public static boolean addPassenger(Person person, Flight flight){
         if (flight.getCapacity() > flight.getCount() +1){
-            flight.passengers[]
+            flight.setPassengers(person, flight.getCount());
+            return true;
+        }else{
+            return false;
         }
     };
 
-    public static boolean removePassenger(){
-
+    public static boolean removePassenger(Person person, Flight flight){
+        Object[] currentPassengers = flight.getPassengers();
+        if()
     };
 
     public static boolean changeStatus{
