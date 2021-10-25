04cd9aa02cffc4c3833e93955b98ee0f71a40d00
helboi4 <h.hopecarter@gmail.com>
2021-10-25 16:00:18 +0200

    final

--- src/main/java/com/airhoot/Main.java
+++ src/main/java/com/airhoot/Main.java
@@ -172,6 +172,7 @@ public class Main {
                                 case 2:
                                     break;
                             }
+                            break;
                         }while(true);
 
 
